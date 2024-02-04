46f27d1ca9195f2491f3cc144b1d43ec240de260
Elian06 <elian.cecchetti@gmail.com>
2024-02-04 22:58:31 +0100

    Ajout des Classes Armes et Décors

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from Personnage import Personnage
+from Decors import Decors
+from Arme import Arme
 
 #exemple d'utilisation de la classe Personnage
+
+decors1 = Decors("Jungle", "Forêt tropicale", 3.55 )
+
+arme1 = Arme("Épée légendaire", "", )
 
 joueur1 = Personnage("Aragorn", "Guerrier", 10, 100, 15, 5)
 joueur2 = Personnage("Gandalf", "Magicien", 10, 80, 5, 15)
